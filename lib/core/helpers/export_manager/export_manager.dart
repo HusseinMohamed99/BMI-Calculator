b27f98112ddeaf3a52e672ad753a98e34cefe4a8
HusseinMohamed99 <husseinhtm99@gmail.com>
2024-10-24 14:50:10 +0300

    Update Gradle Wrapper and Refactor Import Statements
    - Updated the Gradle Wrapper to version 8.8 in `gradle-wrapper.properties`.
    - Refactored import statements in `export_manager.dart` to remove unused imports and improve readability.
    - Moved import statements from `export_manager.dart` to `main.dart` to centralize dependencies.
    - Added error handling for `requestReview` and `goToApplicationOnPlayStore` functions in `main.dart`.
    - Updated `widget_test.dart` to import from `main.dart` instead of `export_manager.dart`.

--- lib/core/helpers/export_manager/export_manager.dart
+++ lib/core/helpers/export_manager/export_manager.dart
@@ -1,22 +1,12 @@
-import 'dart:io';
-
 import 'package:bmi_calculator/cubit/bmi_cubit.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:in_app_review/in_app_review.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-import 'package:upgrader/upgrader.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../../generated/assets.dart';
 import '../../../modules/bmi_result_screen.dart';
-import '../../../modules/bmi_screen.dart';
 
-part './../../../main.dart';
 part './../../../modules/widgets/custom_age_and_weight.dart';
 part './../../../modules/widgets/custom_button_calculator.dart';
 part './../../../modules/widgets/custom_gender.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
-part of './core/helpers/export_manager/export_manager.dart';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:bmi_calculator/core/helpers/export_manager/export_manager.dart';
+import 'package:bmi_calculator/cubit/bmi_cubit.dart';
+import 'package:bmi_calculator/modules/bmi_screen.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:in_app_review/in_app_review.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:upgrader/upgrader.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
   if (kReleaseMode) {
-    await requestReview();
+    try {
+      await requestReview();
+    } catch (e) {
+      log('Error requesting review: $e');
+    }
   }
   if (kDebugMode) {
     await Upgrader.clearSavedSettings();
@@ -58,15 +75,18 @@ Future<void> requestReview() async {
   }
 }
 
-goToApplicationOnPlayStore() async {
-  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+Future<void> goToApplicationOnPlayStore() async {
+  try {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    String packageName = packageInfo.packageName;
+    String url = 'https://play.google.com/store/apps/details?id=$packageName';
 
-  String url = '';
-  String packageName = packageInfo.packageName;
-  if (Platform.isAndroid) {
-    url = 'https://play.google.com/store/apps/details?id=$packageName';
-  } else if (!await launchUrl(Uri.parse(url),
-      mode: LaunchMode.externalApplication)) {
-    throw Exception('Could not launch $url');
+    if (Platform.isAndroid &&
+        !await launchUrl(Uri.parse(url),
+            mode: LaunchMode.externalApplication)) {
+      throw Exception('Could not launch $url');
+    }
+  } catch (e) {
+    log('Error launching URL: $e');
   }
 }
